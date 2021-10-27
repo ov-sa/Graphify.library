127a2511859f058023a934249c1794e3e8e9219b
Aviril <89867985+Aviril@users.noreply.github.com>
2021-10-27 23:51:34 +0300

    Update rt_input_controlmap.lua

--- [Library]/graphify_library/files/shaders/world/no_vs/rt_input_controlmap.lua
+++ [Library]/graphify_library/files/shaders/world/no_vs/rt_input_controlmap.lua
@@ -59,9 +59,11 @@ texture emissiveLayer <string renderTarget = "yes";>;
 -------------------*/
 
 bool disableNormals = false;
-bool enableBump = false;
+bool enableNormalMap = false;
+bool enableBumpMap = false;
 bool enableFilterOverlay = false;
 float4 filterColor;
+texture normalTexture;
 texture bumpTexture;
 float anisotropy = 1;
 float redControlScale = 1;
@@ -94,6 +96,13 @@ sampler controlSampler = sampler_state {
     MipFilter = Linear;
     MaxAnisotropy = gCapsMaxAnisotropy*anisotropy;
     MinFilter = Anisotropic;
+};
+
+sampler normalSampler = sampler_state {
+    Texture = (normalTexture);
+    MinFilter = Linear;
+    MagFilter = Linear;
+    MipFilter = Linear;
 };
 
 sampler bumpSampler = sampler_state {
@@ -148,10 +157,11 @@ Pixel PixelShaderFunction(PSInput PS) {
     sampledControlTexel = lerp(sampledControlTexel, blueTexel, controlTexel.b);
     sampledControlTexel.rgb = sampledControlTexel.rgb/3;
 
-    if (enableBump) {
+    if (enableBumpMap) {
         float4 bumpTexel = tex2D(bumpSampler, PS.TexCoord);
         sampledControlTexel.rgb *= bumpTexel.rgb;
     }
+
     float4 worldColor = sampledControlTexel;
     if (enableFilterOverlay) {
         worldColor += filterColor;
